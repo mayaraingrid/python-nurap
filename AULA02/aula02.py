2258ce4a53fe1f2809fc231c6a5d9b3e22eddb42
mayaraingrid <mayaraingriddossantos4@gmail.com>
2024-12-03 09:57:29 -0300

    Add files via upload

---
+++ AULA02/aula02.py
@@ -0,0 +1,6 @@
+print("olá, boa tarde")
+nome_usuario="carlos vinicius" #tipo -> str = string
+idade_usuario = 18 # int = integer
+altura_usuario = 1.72 # tipo -> float =  ponto flutuante
+maior_idade = True # tipo -> bool = boolean
+print(("olá, boa tarde"), nome_usuario , ("sua idade"), idade_usuario,("sua altura"), altura_usuario, ("você é maior de idade?"), maior_idade)
